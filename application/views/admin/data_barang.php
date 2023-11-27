01e87b379de8556196c46af0d2dab320a3b1a248
Akmal <akmalsulthonf@gmail.com>
2023-11-27 20:09:48 +0700

    add preview button

--- application/views/admin/data_barang.php
+++ application/views/admin/data_barang.php
@@ -1,5 +1,6 @@
 <div class="container-fluid">
   <button class="btn btn-sm btn-primary mb-3" data-toggle="modal" data-target="#tambah_barang"> <i class="fas fa-plus mr-2"></i>Tambah Film</button>
+  <a class="btn btn-sm btn-secondary mb-3" href="<?php echo base_url('') ?>"> <i class="fa-solid fa-eye mr-2"></i>Pratinjau</a>
   <table class="table table-bordered">
     <tr class="text-center">
       <th>No.</th>
