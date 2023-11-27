07a0f30c977d09f5a7c20c7999a8e5809571f727
Akmal <akmalsulthonf@gmail.com>
2023-11-27 22:48:49 +0700

    fix ocd

--- application/views/reward.php
+++ application/views/reward.php
@@ -7,16 +7,16 @@
     <div class="col">
       <a href="https://intip.in/JanganMenyerah"><img src="https://media.21cineplex.com/webcontent/gallery/pictures/169994681848214_240x360.jpg" alt=""></a>
     </div>
-    <div class="col">
+    <div class="col text-right">
       <a href="https://intip.in/JanganMenyerah"><img src="https://media.21cineplex.com/webcontent/gallery/pictures/169962713487308_240x360.jpg" alt=""></a>
     </div>
-    <div class="col">
+    <div class="col text-right">
       <a href="https://intip.in/JanganMenyerah"><img src="https://media.21cineplex.com/webcontent/gallery/pictures/170055044320998_240x360.jpg" alt=""></a>
     </div>
   </div>
   <div class="row mt-5 mb-5">
     <div class="col">
-        <a href="https://intip.in/JanganMenyerah"><img src="https://asset.tix.id/banner_promo_v2/26f19ccf-1739-431c-980f-c594020ef050.jpeg" alt="" width="1160px"></a>
+      <a href="https://intip.in/JanganMenyerah"><img src="https://asset.tix.id/banner_promo_v2/26f19ccf-1739-431c-980f-c594020ef050.jpeg" alt="" width="100%"></a>
     </div>
   </div>
 </div>
